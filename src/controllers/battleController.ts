6caf830b07217a218240f3be010566741f8be685
cassandra-mn <arthenciac@gmail.com>
2022-07-07 13:03:17 -0300

    chore: add structure

---
+++ src/controllers/battleController.ts
@@ -0,0 +1,6 @@
+import {Request, Response} from 'express';
+
+export async function postBattle(req: Request, res: Response) {
+    
+    res.sendStatus(501);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express, {json} from 'express';
 import cors from 'cors';
 
+import router from './routes/index.js';
+
 const app = express();
 
 app.use(cors());
 app.use(json());
+
+app.use(router);
 
 app.listen(6000, () => {
     console.log('Running on 6000');
