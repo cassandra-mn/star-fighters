46ce9aa79eac6a2fdfd859e415f1c602532dcf32
cassandra-mn <arthenciac@gmail.com>
2022-07-07 16:01:41 -0300

    feat: add battle with winner and loser

--- src/services/battleService.ts
+++ src/services/battleService.ts
@@ -1,11 +1,24 @@
 import * as battleRepository from '../repositories/battleRepository.js';
 
 export async function getBattle(firstUser: string, secondUser: string) {
-
     const starsUser1 = await battleRepository.getStars(firstUser);
     const starsUser2 = await battleRepository.getStars(secondUser);
 
-    console.log(starsUser1, starsUser2);
+    const battle = starsUser1 > starsUser2 ? {
+        winner: firstUser,
+        loser: secondUser,
+        draw: false
+    } : starsUser1 < starsUser2 ? {
+        winner: secondUser,
+        loser: firstUser,
+        draw: false
+    } : {
+        winner: null,
+        loser: null,
+        draw: true
+    }
 
-    return {starsUser1, starsUser2};
+    console.log(battle);
+
+    return battle;
 }
